aca6fe15d3a0ef7b7e836c3f989230383fe2d9f7
Junting Chen <54788797+juntingchen-95@users.noreply.github.com>
2020-01-26 22:39:36 -0700

    Update EchoServer.java

--- a0/EchoServer.java
+++ a0/EchoServer.java
@@ -6,13 +6,18 @@ import java.net.Socket;
 
 public class EchoServer {
 
+    // The main method of the Echo Server.
     public static void main(String []args) throws IOException {
+        // Open a socket port at server part.
         ServerSocket server = new ServerSocket(1234);
         Socket client;
+        // The server loop.
         while (true) {
             System.out.println("Waiting for connections from client.");
+            // Wait the client connection.
             client = server.accept();
             System.out.println("Client (IP: " + client.getInetAddress().getHostAddress() +") connected.");
+            // If a client connected, start a new thread.
             EchoThread echoThread = new EchoThread(client);
             Thread clientThread = new Thread(echoThread);
             clientThread.start();
