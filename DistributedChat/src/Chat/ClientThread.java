b8ad5c2eed1e19b7fd2622645b2a6d61c6b691ad
Junting Chen <54788797+juntingchen-95@users.noreply.github.com>
2020-02-26 20:19:23 -0700

    Update ClientThread.java

--- DistributedChat/src/Chat/ClientThread.java
+++ DistributedChat/src/Chat/ClientThread.java
@@ -11,9 +11,13 @@ public class ClientThread implements Runnable {
 
     private Thread clientThread;
     private Socket socket;
+    private ChatNode.NodeInfo successorNode;
 
     public ClientThread(String ipAddress, int port) throws IOException {
         socket = new Socket(ipAddress, port);
+        successorNode = new ChatNode.NodeInfo();
+        successorNode.setIpAddress(ipAddress);
+        successorNode.setPort(port);
     }
 
     public void start() {
@@ -50,8 +54,52 @@ public class ClientThread implements Runnable {
                                 case "connectNewNode":
                                     socket.shutdownOutput();
                                     socket.close();
-                                    ChatNode.NodeInfo nodeInfo = ((MessageUtility) message).getNodeInfo();
-                                    socket = new Socket(nodeInfo.getIpAddress(), nodeInfo.getPort());
+                                    ChatNode.NodeInfo newNodeInfo = ((MessageUtility) message).getNodeInfo();
+                                    socket = new Socket(newNodeInfo.getIpAddress(), newNodeInfo.getPort());
+                                    objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                                    objectOutputStream.writeObject(new MessageUtility("connectNewNode2", successorNode));
+                                    objectOutputStream.flush();
+                                    successorNode = newNodeInfo;
+                                    break;
+                                case "connectNewNode2":
+                                    synchronized (ChatNode.nodeInfo) {
+                                        if (!ChatNode.nodeInfo.getInitializeStatus()) {
+                                            ChatNode.nodeInfo.setIpAddress(((MessageUtility) message).getNodeInfo().getIpAddress());
+                                            ChatNode.nodeInfo.setInitializeStatus(true);
+                                        }
+                                    }
+                                    socket.shutdownOutput();
+                                    socket.close();
+                                    newNodeInfo = ((MessageUtility) message).getNodeInfo();
+                                    socket = new Socket(newNodeInfo.getIpAddress(), newNodeInfo.getPort());
+                                    objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                                    successorNode = newNodeInfo;
+                                    break;
+                                case "selfLeave":
+                                    objectOutputStream.writeObject(
+                                            new MessageLeave("leave", ((MessageUtility) message).getNodeInfo(), successorNode)
+                                    );
+                                    objectOutputStream.flush();
+                                    reConnect = true;
+                                    quit = true;
+                                    break;
+                                case "leave":
+                                    ChatNode.NodeInfo leaveNodeInfo = ((MessageLeave) message).getNodeInfo();
+                                    System.out.println("get leave test");
+                                    if (leaveNodeInfo.getDescription().equals(successorNode.getDescription())) {
+                                        System.out.println("get leave");
+                                        objectOutputStream.writeObject(new MessageUtility("quit", successorNode));
+                                        objectOutputStream.flush();
+                                        socket.shutdownOutput();
+                                        socket.close();
+                                        newNodeInfo = ((MessageLeave) message).getSuccessorNodeInfo();
+                                        socket = new Socket(newNodeInfo.getIpAddress(), newNodeInfo.getPort());
+                                        successorNode = newNodeInfo;
+                                    } else {
+                                        objectOutputStream.writeObject(message);
+                                        objectOutputStream.flush();
+                                    }
+                                    break;
                             }
                         }
                     }
